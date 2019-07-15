0b283d5e5648c6730f2cc4cc745672baea4f13aa
Shelley Arnold <shelle.kay@gmail.com>
2019-07-14 21:04:14 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQed_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Restriction/Filtering Operations
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            IEnumerable<string> LFruits = from fruit in fruits
+                                          where fruit.StartsWith("l")
+                                          select fruit;
+            foreach (var fruit in LFruits)
+            {
+                Console.WriteLine($"L Fruits {fruit}");
+            }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(x => (x % 4 == 0) || (x % 6 == 0));
+            foreach (var n in fourSixMultiples)
+            {
+                Console.WriteLine($"Multiples of 4 and 6 {n}");
+            }
+
+            //Ordering Operations
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                "Francisco", "Tre"
+            };
+
+            List<string> descend = names.OrderByDescending(x => x).ToList();
+            foreach (var name in descend)
+            {
+                Console.WriteLine($"Decending order of words {name}");
+            }
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> numbers2 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+            List<int> ascend = numbers2.OrderBy(x => x).ToList();
+            foreach (var n in ascend)
+            {
+                Console.WriteLine($"Ascending order of numbers {n}");
+            }
+
+            //Aggregate Operations
+            // Output how many numbers are in this list
+            List<int> numbers3 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+            Console.WriteLine($"Number of items in a list {numbers3.Count()}");
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+            Console.WriteLine($"Sum of purhcases {purchases.Sum()}");
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+            Console.WriteLine($"Most espensivest {prices.Max()}");
+
+
+            //Partitioning Operations
+            /*
+                Store each number in the following List until a perfect square
+                is detected.
+
+                Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            */
+            List<int> wheresSquaredo = new List<int>()
+            {
+                66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            };
+
+            // int sqrtResult = wheresSquaredo.Math.Sqrt();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Practice: LINQed List
+
+## Instructions
+Given the collections of items shown below, use LINQ to build the requested collection, and then ```Console.WriteLine()``` each item in that resulting collection.
+
+
+### Results will appear in the console
